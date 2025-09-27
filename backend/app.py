b78e8206e29e1864bb57f1025f728cabb0ae031c
rickpatking <patrickking@ufl.edu>
2025-09-27 14:16:45 -0400

    imports

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, date
+from functools import wraps
+import jwt
+import os
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date
+from sqlalchemy.orm import relationship
+from sqlalchemy import UniqueConstraint
 
+db = SQLAlchemy()
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('')"
